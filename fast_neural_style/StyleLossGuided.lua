0be6162a2ba0057182bf496faed7503fea1d350f
dypromise <nanhangdingyang@163.com>
2019-03-20 18:15:48 +0800

    first commit

--- fast_neural_style/StyleLossGuided.lua
+++ fast_neural_style/StyleLossGuided.lua
@@ -1,101 +1,101 @@
-require 'torch'
-require 'nn'
+require "torch"
+require "nn"
 
-require 'fast_neural_style.GramMatrixGuided'
+require "fast_neural_style.GramMatrixGuided"
 
-local StyleLossGuided, parent = torch.class('nn.StyleLossGuided', 'nn.Module')
-
+local StyleLossGuided,
+  parent = torch.class("nn.StyleLossGuided", "nn.Module")
 
 function StyleLossGuided:__init(strength, loss_type)
-    parent.__init(self)
-    self.strength = strength or 1.0
-    self.loss = 0
-    self.targets = {}
-    self.agg = {} 
-    self.agg_out = nil
+  parent.__init(self)
+  self.strength = strength or 1.0
+  self.loss = 0
+  self.targets = {}
+  self.agg = {}
+  self.agg_out = nil
 
-    self.mode = 'none'
-    loss_type = loss_type or 'L2'
-    if loss_type == 'L2' then
-        self.crit = nn.MSECriterion()
-    elseif self.crit == 'SmoothL1' then
-        self.crit = nn.SmoothL1Criterion()
-    else
-        error(string.format('invalid loss type "%s"', loss_type))
-    end
+  self.mode = "none"
+  loss_type = loss_type or "L2"
+  if loss_type == "L2" then
+    self.crit = nn.MSECriterion()
+  elseif self.crit == "SmoothL1" then
+    self.crit = nn.SmoothL1Criterion()
+  else
+    error(string.format('invalid loss type "%s"', loss_type))
+  end
 end
-
 
 function StyleLossGuided:updateOutput(input)
-    local features, guides = input[1],input[2]
-    assert(features:dim() == guides:dim())
-    local n_guides = nil
-    local dtype = features:type()
+  local features,
+    guides = input[1], input[2]
+  assert(features:dim() == guides:dim())
+  local n_guides = nil
+  local dtype = features:type()
+  if guides:dim() == 3 then
+    n_guides = guides:size()[1]
+  elseif guides:dim() == 4 then
+    n_guides = guides:size()[2]
+  end
+  if self.mode == "capture" then
+    assert(guides:dim() == 4, "style image and guide should have 4 dimensions")
+    for i = 1, n_guides do
+      self.agg[i] = nn.GramMatrixGuided():type(dtype)
+      self.targets[i] =
+        self.agg[i]:forward({features, guides[{{}, {i}, {}, {}}]:expandAs(features):contiguous()}):clone()
+    end
+  elseif self.mode == "loss" then
+    self.loss = 0
     if guides:dim() == 3 then
-       n_guides = guides:size()[1] 
+      for i = 1, n_guides do
+        local agg_out = self.agg[i]:forward({features, guides[{{i}, {}, {}}]:expandAs(features):contiguous()})
+        self.loss = self.loss + self.strength * self.crit(agg_out, self.targets[i])
+      end
     elseif guides:dim() == 4 then
-       n_guides = guides:size()[2]
+      for i = 1, n_guides do
+        local agg_out = self.agg[i]:forward({features, guides[{{}, {i}, {}, {}}]:expandAs(features):contiguous()})
+        self.loss = self.loss + self.strength * self.crit(agg_out, self.targets[i]:expandAs(agg_out):contiguous())
+      end
     end
-    if self.mode == 'capture' then
-        assert(guides:dim() == 4, 'style image and guide should have 4 dimensions')
-            for i = 1, n_guides do
-                self.agg[i] = nn.GramMatrixGuided():type(dtype)
-                self.targets[i] = self.agg[i]:forward({features, guides[{{},{i},{},{}}]:expandAs(features):contiguous()}):clone()
-            end
-    elseif self.mode == 'loss' then
-        self.loss = 0
-        if guides:dim() == 3 then
-            for i = 1, n_guides do
-                local agg_out = self.agg[i]:forward({features, guides[{{i},{},{}}]:expandAs(features):contiguous()})
-                self.loss = self.loss + self.strength * self.crit(agg_out, self.targets[i])
-            end
-        elseif guides:dim() == 4 then
-            for i = 1, n_guides do
-                local agg_out = self.agg[i]:forward({features, guides[{{},{i},{},{}}]:expandAs(features):contiguous()})
-                self.loss = self.loss + self.strength * self.crit(agg_out, self.targets[i]:expandAs(agg_out):contiguous())
-            end
-        end
-    end
-    self.output = input
-    return self.output
+  end
+  self.output = input
+  return self.output
 end
-
 
 function StyleLossGuided:updateGradInput(input, gradOutput)
-    local features, guides = input[1],input[2]
-    local n_guides = nil
+  local features,
+    guides = input[1], input[2]
+  local n_guides = nil
+  if guides:dim() == 3 then
+    n_guides = guides:size()[1]
+  elseif guides:dim() == 4 then
+    n_guides = guides:size()[2]
+  end
+  if self.mode == "capture" or self.mode == "none" then
+    self.gradInput = gradOutput
+  elseif self.mode == "loss" then
+    self.gradInput = gradOutput
     if guides:dim() == 3 then
-       n_guides = guides:size()[1] 
+      for i = 1, n_guides do
+        self.crit:backward(self.agg[i].output, self.targets[i])
+        self.crit.gradInput:mul(self.strength)
+        self.agg[i]:backward({features, guides[{{i}, {}, {}}]:expandAs(features):contiguous()}, self.crit.gradInput)
+        self.gradInput[1]:add(self.agg[i].gradInput)
+      end
     elseif guides:dim() == 4 then
-       n_guides = guides:size()[2]
+      for i = 1, n_guides do
+        self.crit:backward(self.agg[i].output, self.targets[i]:expandAs(self.agg[i].output))
+        self.crit.gradInput:mul(self.strength)
+        self.agg[i]:backward({features, guides[{{}, {i}, {}, {}}]:expandAs(features):contiguous()}, self.crit.gradInput)
+        self.gradInput[1]:add(self.agg[i].gradInput)
+      end
     end
-    if self.mode == 'capture' or self.mode == 'none' then
-        self.gradInput = gradOutput
-    elseif self.mode == 'loss' then
-        self.gradInput = gradOutput
-        if guides:dim() == 3 then
-            for i = 1, n_guides do
-                self.crit:backward(self.agg[i].output, self.targets[i])
-                self.crit.gradInput:mul(self.strength)
-                self.agg[i]:backward({features, guides[{{i},{},{}}]:expandAs(features):contiguous()}, self.crit.gradInput)
-                self.gradInput[1]:add(self.agg[i].gradInput)
-            end
-        elseif guides:dim() == 4 then
-            for i = 1, n_guides do
-                self.crit:backward(self.agg[i].output, self.targets[i]:expandAs(self.agg[i].output))
-                self.crit.gradInput:mul(self.strength)
-                self.agg[i]:backward({features, guides[{{},{i},{},{}}]:expandAs(features):contiguous()}, self.crit.gradInput)
-                self.gradInput[1]:add(self.agg[i].gradInput)
-            end
-        end
-    end
-    return self.gradInput
+  end
+  return self.gradInput
 end
 
-
 function StyleLossGuided:setMode(mode)
-    if mode ~= 'capture' and mode ~= 'loss' and mode ~= 'none' then
-        error(string.format('Invalid mode "%s"', mode))
-    end
-    self.mode = mode
+  if mode ~= "capture" and mode ~= "loss" and mode ~= "none" then
+    error(string.format('Invalid mode "%s"', mode))
+  end
+  self.mode = mode
 end
--- webcam_demo.lua
+++ webcam_demo.lua
@@ -1,45 +1,44 @@
-require 'torch'
-require 'nn'
-require 'image'
-require 'camera'
+require "torch"
+require "nn"
+require "image"
+require "camera"
 
-require 'qt'
-require 'qttorch'
-require 'qtwidget'
+require "qt"
+require "qttorch"
+require "qtwidget"
 
-require 'fast_neural_style.ShaveImage'
-require 'fast_neural_style.TotalVariation'
-require 'fast_neural_style.InstanceNormalization'
-local utils = require 'fast_neural_style.utils'
-local preprocess = require 'fast_neural_style.preprocess'
-
+require "fast_neural_style.ShaveImage"
+require "fast_neural_style.TotalVariation"
+require "fast_neural_style.InstanceNormalization"
+local utils = require "fast_neural_style.utils"
+local preprocess = require "fast_neural_style.preprocess"
 
 local cmd = torch.CmdLine()
 
 -- Model options
-cmd:option('-models', 'models/instance_norm/candy.t7')
-cmd:option('-height', 480)
-cmd:option('-width', 640)
+cmd:option("-models", "models/instance_norm/candy.t7")
+cmd:option("-height", 480)
+cmd:option("-width", 640)
 
 -- GPU options
-cmd:option('-gpu', -1)
-cmd:option('-backend', 'cuda')
-cmd:option('-use_cudnn', 1)
+cmd:option("-gpu", -1)
+cmd:option("-backend", "cuda")
+cmd:option("-use_cudnn", 1)
 
 -- Webcam options
-cmd:option('-webcam_idx', 0)
-cmd:option('-webcam_fps', 60)
-
+cmd:option("-webcam_idx", 0)
+cmd:option("-webcam_fps", 60)
 
 local function main()
   local opt = cmd:parse(arg)
 
-  local dtype, use_cudnn = utils.setup_gpu(opt.gpu, opt.backend, opt.use_cudnn == 1)
+  local dtype,
+    use_cudnn = utils.setup_gpu(opt.gpu, opt.backend, opt.use_cudnn == 1)
   local models = {}
 
   local preprocess_method = nil
-  for _, checkpoint_path in ipairs(opt.models:split(',')) do
-    print('loading model from ', checkpoint_path)
+  for _, checkpoint_path in ipairs(opt.models:split(",")) do
+    print("loading model from ", checkpoint_path)
     local checkpoint = torch.load(checkpoint_path)
     local model = checkpoint.model
     model:evaluate()
@@ -48,14 +47,14 @@ local function main()
       cudnn.convert(model, cudnn)
     end
     table.insert(models, model)
-    local this_preprocess_method = checkpoint.opt.preprocessing or 'vgg'
+    local this_preprocess_method = checkpoint.opt.preprocessing or "vgg"
     if not preprocess_method then
-      print('got here')
+      print("got here")
       preprocess_method = this_preprocess_method
       print(preprocess_method)
     else
       if this_preprocess_method ~= preprocess_method then
-        error('All models must use the same preprocessing')
+        error("All models must use the same preprocessing")
       end
     end
   end
@@ -66,7 +65,7 @@ local function main()
     idx = opt.webcam_idx,
     fps = opt.webcam_fps,
     height = opt.height,
-    width = opt.width,
+    width = opt.width
   }
   local cam = image.Camera(camera_opt)
 
@@ -76,7 +75,8 @@ local function main()
     local img = cam:forward()
 
     -- Preprocess the frame
-    local H, W = img:size(2), img:size(3)
+    local H,
+      W = img:size(2), img:size(3)
     img = img:view(1, 3, H, W)
     local img_pre = preprocess.preprocess(img):type(dtype)
 
@@ -89,13 +89,13 @@ local function main()
       local img_out = preprocess.deprocess(img_out_pre)[1]:float()
       table.insert(imgs_out, img_out)
     end
-    local img_disp = image.toDisplayTensor{
+    local img_disp =
+      image.toDisplayTensor {
       input = imgs_out,
       min = 0,
       max = 1,
-      nrow = math.floor(math.sqrt(#imgs_out)),
+      nrow = math.floor(math.sqrt(#imgs_out))
     }
-
 
     if not win then
       -- On the first call use image.display to construct a window
@@ -110,6 +110,4 @@ local function main()
   end
 end
 
-
 main()
-
