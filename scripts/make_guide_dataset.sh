e9bcea4230bb181f694f53fba5585303201246d1
dypromise <nanhangdingyang@163.com>
2019-04-09 16:42:21 +0800

    update make_dataset

--- scripts/make_guide_dataset.sh
+++ scripts/make_guide_dataset.sh
@@ -4,7 +4,7 @@ python make_guide_dataset.py \
     --train_guides_dir /media/bigdrive/dingyang/our_faces_guides \
     --val_dir /media/bigdrive/dingyang/our_faces_val/ \
     --val_guides_dir /media/bigdrive/dingyang/our_faces_val_guides/ \
-    --output_file /media/bigdrive/dingyang/ourfaces_512.hdf5 \
+    --output_file /media/bigdrive/dingyang/our_faces_512.hdf5 \
     --height 512 \
     --width 512 \
     --num_guides 3
---
+++ scripts/seg2guide.sh
@@ -0,0 +1,4 @@
+python ./seg2guide.py \
+    --input_image ~/pt_test/hd_ori_label.png \
+    --output_guide_dir ~/pt_test/hd_ori_guides \
+    --output_hdf5_path ~/pt_test/hd_ori_guide.hdf5
